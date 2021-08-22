d4ea0f18914617566f14689d0311f583f18b199c
Miryam <mbathilde@gmail.com>
2021-08-22 14:07:58 +0200

    creamos marcador en el mapa con agm marker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { AgmCoreModule } from "@agm/core";
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,7 @@ import { MapaComponent } from './mapa/mapa.component';
     })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -6,12 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
+  latitud: number;
+  longitud: number;
 
-  constructor() { }
+  constructor() {
+    this.latitud = 40;
+    this.longitud = -3;
+  }
 
   ngOnInit(): void {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
+      this.latitud = position.coords.latitude;
+      this.longitud = position.coords.longitude;
+      //console.log(position);
     })
   }
 }
