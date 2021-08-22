bff429c9b3e0ab703cdc17bd1d0e9edc3331ccfa
Miryam <mbathilde@gmail.com>
2021-08-22 13:11:36 +0200

    implementamos API en mapa con navigator geolocation getCurrentPosition

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -10,6 +10,8 @@ export class MapaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    })
   }
-
 }
