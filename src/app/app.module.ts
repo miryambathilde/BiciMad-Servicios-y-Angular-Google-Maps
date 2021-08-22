12b7bcfa0fdd2b5b73372a3af7a802d01979794a
Miryam <mbathilde@gmail.com>
2021-08-22 14:17:26 +0200

    importo HttpClient Module para peticiones http y genero servicio API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { AgmCoreModule } from "@agm/core";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapaComponent } from './mapa/mapa.component';
@@ -13,7 +13,9 @@ import { MapaComponent } from './mapa/mapa.component';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBXoe3vvdGGosbpLVZqUncQDgiW4UAbl58'
-    })
+    }),
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
